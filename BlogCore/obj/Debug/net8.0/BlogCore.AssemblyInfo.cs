f146b1dbbd4c88b13a4dcaa6135237a57d65cd63
CescDev <medicalsos068@gmail.com>
2024-11-13 13:35:43 -0600

    Creando estructura del proyecto y rutas

--- BlogCore/obj/Debug/net8.0/BlogCore.AssemblyInfo.cs
+++ BlogCore/obj/Debug/net8.0/BlogCore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b87eaec5e3f925f182a737c274e88f72e96bc16")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
