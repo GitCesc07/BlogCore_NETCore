9b87eaec5e3f925f182a737c274e88f72e96bc16
César Urroz <cesar04071997@gmail.com>
2024-11-13 00:27:12 -0600

    Proyecto BlogCore ASP.NET Core

---
+++ BlogCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Poner aquí todos los modelos que se vayan creando
+    }
+}
